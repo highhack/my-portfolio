8ea9bff079667c6f7194fd69f31057bdafb6761b
highhack <forgreatperson@gmail.com>
2021-05-28 16:28:36 +0300

    added skills

--- src/skills/Skill.js
+++ src/skills/Skill.js
@@ -4,10 +4,12 @@ import s from './Skill.module.scss'
 function Skill(props) {
     return (
         <div className={s.skill}>
-            <div className={s.icon}>
-                <img alt={''} className={s.imgSkill} src={props.img}/>
+            <div className={s.imgWithTitle}>
+                <div className={s.icon}>
+                    <img alt={''} className={s.imgSkill} src={props.img}/>
+                </div>
+                <h3 className={s.skillTitle}>{props.title}</h3>
             </div>
-            <h3 className={s.skillTitle}>{props.title}</h3>
             <span className={s.description}>{props.description}</span>
         </div>
     );
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -5,18 +5,24 @@ import Title from "../Title/Title";
 import js from '../common/icons/icons8-javascript-100.png'
 import reactjs from '../common/icons/icons8-react-native-100.png'
 import reduxjs from '../common/icons/icons8-redux-100.png'
+import restApi from '../common/icons/rest-api-icon.png'
+import materialIcon from '../common/icons/material-ui-pngrepo-com.png'
+import typescriptIcon from '../common/icons/typescript.png'
+import sassIcon from '../common/icons/sass.png'
+import gitIcon from '../common/icons/git-branch-skill.png'
+import storyBookIcon from '../common/icons/storybook.png'
 import Fade from 'react-reveal/Fade';
 
 function Skills() {
     return (
         <div id={'Skills'} className={s.skills}>
             <div className={s.container}>
-                <Title title={'SKILLS'} />
+                <Title title={'SKILLS'}/>
                 <Fade bottom>
                     <div className={s.skillsItems}>
                         <Skill title={'JS'}
                                img={js}
-                               description={'I am working with css and javascript frameworks.' +
+                               description={'I am working with  JS and CSS frameworks.' +
                                ' Debugging, testing, analyzing and removing errors'}/>
                         <Skill title={'React'}
                                img={reactjs}
@@ -26,7 +32,29 @@ function Skills() {
                                    'Replacing application code with hooks'}/>
                         <Skill title={'Redux'}
                                img={reduxjs}
-                               description={'I have a confident understanding of redux for creating large projects'}/>
+                               description={'I have a confident understanding of Redux for creating large projects'}/>
+                        <Skill title={'Rest API'}
+                               img={restApi}
+                               description={'I am using Rest API to request data from server and get response'}/>
+                        <Skill title={'Typescript'}
+                               img={typescriptIcon}
+                               description={'I mostly use TS instead of JS. It allows me to avoid' +
+                               ' a lot of programming errors and speed up the development process.'}/>
+                        <Skill title={'Git'}
+                               img={gitIcon}
+                               description={'Joint projects where I was part of the team were written via Git'}/>
+                        <Skill title={'StoryBook'}
+                               img={storyBookIcon}
+                               description={'Makes it easier for me to test and allows me to debug' +
+                               ' a component with different data.'}/>
+                        <Skill title={'Sass'}
+                               img={sassIcon}
+                               description={'On large projects, I use SASS instead of CSS ' +
+                               'to reduce the number of lines of code and development time.'}/>
+                        <Skill title={'Material UI'}
+                               img={materialIcon}
+                               description={'Thanks to  Material UI in the React , I can quickly create a simply' +
+                               ' and beautiful design.'}/>
                     </div>
                 </Fade>
             </div>
