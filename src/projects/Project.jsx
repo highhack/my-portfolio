62544d4de23350c38e93c7cd4a0fe73335675f52
German Goldin <forgreatperson@gmail.com>
2022-01-14 18:41:50 +0200

    fixed styles

--- src/projects/Project.jsx
+++ src/projects/Project.jsx
@@ -8,6 +8,7 @@ function Project(props) {
                 <a href={props.link} className={s.watch} rel="noreferrer" target="_blank">Site</a>
                 <a href={props.codeLink} className={s.watch} rel="noreferrer" target="_blank">Code</a>
             </div>
+
             <h3 className={s.title}>{props.title}</h3>
             <span className={s.description}>{props.description}</span>
         </div>
